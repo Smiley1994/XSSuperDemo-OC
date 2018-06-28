a407616ff370c7b4c6c308d4a7175a49fd1d64e7
songf <smileysun1994@gmail.com>
2018-06-28 19:58:03 +0800

    first update

---
+++ XSSuperDemo-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XSSuperDemo-OC
+//
+//  Created by Good_Morning_ on 2018/6/28.
+//  Copyright © 2018年 GoodMorning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
