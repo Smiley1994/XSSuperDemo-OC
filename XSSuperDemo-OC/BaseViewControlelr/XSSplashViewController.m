0f450ebf93dbaf4ccf1a33c83901c09ea46bcf4e
SunXiaoSong <sunxiaosong@ann9.com>
2018-07-02 20:55:44 +0800

    delete main and launchScreen

---
+++ XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
@@ -0,0 +1,38 @@
+//
+//  XSSplashViewController.m
+//  XSSuperDemo-OC
+//
+//  Created by 晓松 on 2018/7/2.
+//  Copyright © 2018年 GoodMorning. All rights reserved.
+//
+
+#import "XSSplashViewController.h"
+
+@interface XSSplashViewController ()
+
+@end
+
+@implementation XSSplashViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
