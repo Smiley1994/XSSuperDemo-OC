35129c1c02e28e75e695d2609285fa8fde9d3afc
SunXiaoSong <sunxiaosong@ann9.com>
2018-07-02 21:00:13 +0800

    update

--- XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
+++ XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
@@ -14,25 +14,18 @@
 
 @implementation XSSplashViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    [self.navigationController setNavigationBarHidden:YES];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
     self.view.backgroundColor = UIColor.whiteColor;
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
