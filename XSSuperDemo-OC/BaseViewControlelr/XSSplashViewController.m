0bb9c8b8209cf09285b951a46a605c72511a746d
SunXiaoSong <sunxiaosong@ann9.com>
2018-07-17 14:29:41 +0800

    update

--- XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
+++ XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "XSSplashViewController.h"
+#import "iQiYiPlayButton.h"
+#import "YouKuPlayButton.h"
 
-@interface XSSplashViewController ()
+@interface XSSplashViewController () {
+    
+    iQiYiPlayButton *_iQiYiPlayButton;
+    YouKuPlayButton *_youKuPlayButton;
+}
 
 @end
 
@@ -24,7 +30,36 @@
     
     self.view.backgroundColor = UIColor.whiteColor;
     
+    //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
+    _iQiYiPlayButton = [[iQiYiPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:iQiYiPlayButtonStatePlay];
+    _iQiYiPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height/3);
+    [_iQiYiPlayButton addTarget:self action:@selector(iQiYiPlayMethod) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_iQiYiPlayButton];
     
+    //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
+    _youKuPlayButton = [[YouKuPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:YouKuPlayButtonStatePlay];
+    _youKuPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height*2/3);
+    [_youKuPlayButton addTarget:self action:@selector(youKuPlayMethod) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_youKuPlayButton];
+    
+}
+
+- (void)iQiYiPlayMethod {
+    //通过判断当前状态 切换显示状态
+    if (_iQiYiPlayButton.buttonState == iQiYiPlayButtonStatePause) {
+        _iQiYiPlayButton.buttonState = iQiYiPlayButtonStatePlay;
+    }else {
+        _iQiYiPlayButton.buttonState = iQiYiPlayButtonStatePause;
+    }
+}
+
+- (void)youKuPlayMethod {
+    //通过判断当前状态 切换显示状态
+    if (_youKuPlayButton.buttonState == YouKuPlayButtonStatePause) {
+        _youKuPlayButton.buttonState = YouKuPlayButtonStatePlay;
+    }else {
+        _youKuPlayButton.buttonState = YouKuPlayButtonStatePause;
+    }
 }
 
 
