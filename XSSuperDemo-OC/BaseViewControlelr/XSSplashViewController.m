6b8f4a0ab2107abdff86c9ee3bd8ab6dc2f5d46d
SunXiaoSong <sunxiaosong@ann9.com>
2018-07-23 21:04:05 +0800

    update

--- XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
+++ XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
@@ -10,6 +10,7 @@
 #import "iQiYiPlayButton.h"
 #import "YouKuPlayButton.h"
 #import "XSUIMacro.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface XSSplashViewController () {
     
@@ -36,12 +37,13 @@
     _iQiYiPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height/3);
     [_iQiYiPlayButton addTarget:self action:@selector(iQiYiPlayMethod) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_iQiYiPlayButton];
-    
+
     //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
     _youKuPlayButton = [[YouKuPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:YouKuPlayButtonStatePlay];
     _youKuPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height*2/3);
     [_youKuPlayButton addTarget:self action:@selector(youKuPlayMethod) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_youKuPlayButton];
+
     
 }
 
