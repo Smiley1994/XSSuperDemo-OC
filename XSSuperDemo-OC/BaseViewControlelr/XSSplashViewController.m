eb45c65399d0d5d16c1307f6ee88e95876e72605
SunXiaoSong <sunxiaosong@ann9.com>
2018-11-29 19:35:49 +0800

    update

--- XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
+++ XSSuperDemo-OC/BaseViewControlelr/XSSplashViewController.m
@@ -33,19 +33,19 @@
     
     self.view.backgroundColor = UIColor.whiteColor;
     
-//    //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
-//    _iQiYiPlayButton = [[iQiYiPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:iQiYiPlayButtonStatePlay];
-//    _iQiYiPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height/3);
-//    [_iQiYiPlayButton addTarget:self action:@selector(iQiYiPlayMethod) forControlEvents:UIControlEventTouchUpInside];
-//    [self.view addSubview:_iQiYiPlayButton];
-//
-//    //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
-//    _youKuPlayButton = [[YouKuPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:YouKuPlayButtonStatePlay];
-//    _youKuPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height*2/3);
-//    [_youKuPlayButton addTarget:self action:@selector(youKuPlayMethod) forControlEvents:UIControlEventTouchUpInside];
-//    [self.view addSubview:_youKuPlayButton];
+    //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
+    _iQiYiPlayButton = [[iQiYiPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:iQiYiPlayButtonStatePlay];
+    _iQiYiPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height/3);
+    [_iQiYiPlayButton addTarget:self action:@selector(iQiYiPlayMethod) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_iQiYiPlayButton];
+
+    //创建播放按钮，需要初始化一个状态，即显示暂停还是播放状态
+    _youKuPlayButton = [[YouKuPlayButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60) state:YouKuPlayButtonStatePlay];
+    _youKuPlayButton.center = CGPointMake(self.view.center.x, self.view.bounds.size.height*2/3);
+    [_youKuPlayButton addTarget:self action:@selector(youKuPlayMethod) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_youKuPlayButton];
     
-    [self openRootViewController];
+//    [self openRootViewController];
 }
 
 - (void) openRootViewController {
